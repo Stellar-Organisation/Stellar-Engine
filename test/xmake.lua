0ccde24d1a976536137b9d192f0b0c0dcb3d1e54
Mathéo Martin <matheo.martin@epitech.eu>
2024-01-18 14:06:53 +0100

    Refactor: Remove all cmake and replace by xmake
    MAJOR

---
+++ test/xmake.lua
@@ -0,0 +1,26 @@
+-- Add the packages required
+add_requires("catch2", "boost", "fmt")
+
+-- Create the Stellar-Engine-Test project
+target("test")
+
+    -- Set the project kind to binary
+    set_kind("binary")
+
+    -- Set the target directory to bin/tests
+    set_targetdir("../bin")
+
+    -- Set the C++ standard to C++20
+    set_languages("cxx20")
+
+    -- Add the source files
+    add_files("**.cpp")
+
+    -- Add Stellar-Engine as a dependency
+    add_deps("Stellar-Engine")
+
+    -- Add the include directories
+    add_includedirs("../includes", "../includes/Core", "../includes/Core/Components", "../includes/Core/Events", "../includes/Core/Systems", {public = true})
+
+    -- Add the packages required
+    add_packages("catch2", "boost", "fmt")
---
+++ xmake.lua
@@ -0,0 +1,8 @@
+set_project("Stellar-Engine")
+
+set_toolchains("clang")
+
+add_rules("mode.debug", "mode.release")
+
+includes("src")
+includes("test")
