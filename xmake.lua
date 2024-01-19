f03638013efb0846ac8e0de91c39e18023d795ee
Mathéo Martin <matheo.martin@epitech.eu>
2024-01-19 10:39:31 +0100

    CICD: Fix the compiler for codeql and modify the version of llvm to take the latest of the os
    PATCH

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,6 @@
 set_project("Stellar-Engine")
 
-add_requires("llvm 16.x", {alias = "llvm-16"})
-
-set_toolchains("llvm@llvm-16")
+set_toolchains("clang")
 set_languages("cxx20")
 
 add_rules("mode.debug", "mode.release")
