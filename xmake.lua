e564d1f1ce5f1ba8519b1bfb0c77b1d633ff73aa
Mathéo Martin <matheo.martin@epitech.eu>
2024-01-23 17:45:53 +0100

    CICD: Trying to fix msvc problem
    PATCH

--- xmake.lua
+++ xmake.lua
@@ -1,8 +1,12 @@
 set_project("Stellar-Engine")
 
-add_toolchains("clang")
-set_languages("cxx20")
+if is_plat("windows") then
+    add_toolchains("msvc")
+else
+    add_toolchains("clang")
+end
 
+set_languages("cxx20")
 add_rules("mode.debug", "mode.release")
 
 includes("src")
